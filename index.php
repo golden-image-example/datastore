07ab13f6171ae36284a35b8864e248034e9b2951
Kevin Franklin <kfrankli@redhat.com>
2024-10-02 16:38:32 -0400

    init

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>PHP Test Site</title>
+    </head>
+    <body>
+	<?php echo '<p>Hello World</p>'; ?>
+	<?php $image_url='burg_eltz.php'; ?>
+	<img src="<?php echo $image_url;?>">
+    </body>
+</html>
